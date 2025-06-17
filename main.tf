3cd00e7d269fc2ec525ad42a015a838a48f7e97c
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-06-17 22:40:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "nullresource--2" {
-  #source  = "app.terraform.io/Test-Abhinav/nullresource--2/aws"
-  #version = "1.0.3"
-source = "git::ssh://git@github.com/abhinav-tripathi14/terraform-aws-nullresource--2.git?ref=main"
+  source  = "app.terraform.io/Test-Abhinav/nullresource--2/aws"
+  version = "1.0.3"
+#source = "git::ssh://git@github.com/abhinav-tripathi14/terraform-aws-nullresource--2.git?ref=main"
 }
