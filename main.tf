893bd06752e4ceb929c0d6313de8f688d48463ed
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-06-17 15:38:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ module "nullresource--2" {
   source  = "app.terraform.io/Test-Abhinav/nullresource--2/aws"
   version = "1.0.0"
 }
+resource "null_resource""test"{
+ provisioner "local-exec" {
 command = "bash ${path.root}/scripts/sync-all-appsets.sh"
+}
+}
